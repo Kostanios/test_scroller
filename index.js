3d136395f8de63849e88c2ca170d559da06e6fe1
Kostanios <kotov.const@mail.ru>
2020-11-24 23:36:29 +0300

    feat: test scroll init

---
+++ index.js
@@ -0,0 +1,93 @@
+const { Builder, By, Key, until } = require('selenium-webdriver');
+
+async function connect() {
+    let driver = await new Builder().forBrowser("chrome").build();
+    await driver.manage().window().maximize();
+    await driver.get("http://www.certifications.ru ");
+    openTest( driver )
+}
+
+async function openTest ( driver ) {
+    setTimeout(async ()=>{
+
+        await driver.findElement(By.xpath('/html/body/div[1]/div[3]/div[3]/a[1]')).click()
+        startTest(driver)
+        
+    }, 1000)
+}
+
+async function startTest ( driver ) {
+    setTimeout(async ()=>{
+        try{
+            await (await driver.findElement(By.className('button-blue-long start-testing'))).click()
+            answerMachine(driver)
+        }
+        catch (e) {
+            console.log('error - start test');
+            startTest(driver)
+        }
+        
+    }, 1000)
+    
+}
+///html/body/div[6]/div[1]/div[2]/div[2]/form/div[2]
+async function answerMachine (driver) {
+    setTimeout(async ()=> {
+        try {
+            let elements
+            elements = await (await driver.findElements(By.className('question')));
+            let success
+            let exit_button
+            try{
+                success = await (await driver.findElement(By.className('success')));
+            }
+            catch (e) {
+                console.log("результата ещё нет")
+            }
+            if( elements.length ){
+                const firstClick = parseInt( Math.random() * 4 );
+                const secondClick = parseInt( Math.random() * 4 );
+                const thirdClick = parseInt( Math.random() * 4 );
+                await elements[firstClick].click();
+                await elements[secondClick].click();
+                await elements[thirdClick].click();
+                await (await driver.findElement(By.className('send-answer button-blue-small'))).click()
+                answerMachine(driver)
+                return
+            }
+            // button-blue-small testing-exit
+            else if (success) {
+                success = await (await driver.findElement(By.className('success')));
+                const size = await (await success.getRect())
+                console.log(size.width)
+                  if (size.width < 400){
+                      const close_button = await (await driver.findElement(By.id('DOMWindowClose')))
+                      console.log(close_button)
+                      close_button.click()
+                       setTimeout(async ()=>{
+                           const buttons = await(await driver.findElements(By.className('ui-button-text')))
+                           buttons[0].click()
+                           openTest(driver)
+                      },1000);
+                }
+            }
+            else {
+                exit_button = await(await driver.findElement(By.className('button-blue-small testing-exit')));
+                console.log(exit_button);
+                exit_button.click()
+                setTimeout(async ()=>{
+                    const buttons = await(await driver.findElements(By.className('ui-button-text')))
+                    buttons[0].click()
+                    openTest(driver)
+               },1000);
+            }
+        }
+        catch ( error ) {
+            console.log(error)
+            answerMachine(driver)
+        }
+    }, 100)
+}
+
+connect()
+ 
